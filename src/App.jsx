95cfecd24d48faf79aabd4751e087edd8f0781ac
Najil Rahman PM <sunaidnk1.gmail.com>
2024-12-06 15:36:48 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home'
 import '../bootstrap.min.css'
